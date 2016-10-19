69480442d214d23c312713070ab67a5a48362edb
Aleksey Agapov <aleksey.agapov@saritasa.com>
2016-10-19 19:37:01 +0500

    Started creating sidebar.

--- TactixBoard/Views/Sidebar/MenuType.swift
+++ TactixBoard/Views/Sidebar/MenuType.swift
@@ -51,7 +51,6 @@ class MenuType: UIView {
     }
 
     func loadView(withName nibName: String) -> UIView {
-
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: nibName, bundle: bundle)
 
@@ -60,41 +59,3 @@ class MenuType: UIView {
         return view
     }
 }
-
-/*class DesignableXibView: UIView {
-
- var contentView : UIView?
-
- override init(frame: CGRect) {
- super.init(frame: frame)
- xibSetup()
- }
-
- required init?(coder aDecoder: NSCoder) {
- super.init(coder: aDecoder)
- xibSetup()
- }
-
- func xibSetup() {
- contentView = loadViewFromNib()
-
- // use bounds not frame or it'll be offset
- contentView!.frame = bounds
-
- // Make the view stretch with containing view
- contentView!.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
-
- // Adding custom subview on top of our view (over any custom drawing > see note below)
- addSubview(contentView!)
- }
-
- func loadViewFromNib() -> UIView! {
-
- let bundle = Bundle(for: type(of: self))
- let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
- let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
- 
- return view
- }
- 
- }*/
--- TactixBoard/Views/Sidebar/SidebarMenu.swift
+++ TactixBoard/Views/Sidebar/SidebarMenu.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 
-@IBDesignable
 class SidebarMenu: UIView {
-    private enum Menu {
+    enum Menu {
         case add,
         delete,
         draw,
@@ -39,27 +38,60 @@ class SidebarMenu: UIView {
         }
     }
 
-    private var menuState: [Menu] = [.add,.delete,.draw,.clear,.save,.record,.play]
+    static let defaultState: [Menu] = [.add, .delete, .draw, .clear, .save, .record, .play]
 
-    override func layoutSubviews() {
+    // MARK: - Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        buildMenu()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        buildMenu()
+    }
+
+    // MARK: Public methods
+    func buildMenu(with state: [Menu] = defaultState, animated: Bool = false) {
+        if animated {
+            for view in subviews {
+                UIView.animate(withDuration: 1.0, animations: {
+                    view.alpha = 0.0
+                }) { (finished) in
+                    view.removeFromSuperview()
+                }
+            }
+        }
+
+        for view in subviews {
+            view.removeFromSuperview()
+        }
+
         var previousButton: UIButton?
-        for menu in menuState {
+        for menu in state {
             let button = UIButton()
             button.setImage(menu.image, for: .normal)
+            self.addSubview(button)
             button.snp.makeConstraints({ (make) in
+                make.height.width.equalTo(60)
                 make.leading.trailing.equalToSuperview()
                 make.centerX.equalToSuperview()
                 if previousButton != nil {
-                    make.top.equalTo(previousButton!).offset(8)
+                    make.top.equalTo(previousButton!.snp.bottom)
                 } else {
                     make.top.equalToSuperview()
                 }
 
             })
-            self.addSubview(button)
+            button.addTarget(self, action: #selector(buttonClick(_:)), for: .touchUpInside)
             previousButton = button
         }
     }
 
-
+    func buttonClick(_ sender: UIButton) {
+        print("hello")
+        UIView.animate(withDuration: 1.0) {
+            self.buildMenu(with: [.add,.delete])
+        }
+    }
 }
